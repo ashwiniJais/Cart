23d351474d85ae893af5d74edd24005314207497
ashwiniJais <ashwani.jaiswal01@gmail.com>
2021-03-16 14:31:22 +0530

    Creating a cart List

--- src/CartItem.js
+++ src/CartItem.js
@@ -18,11 +18,30 @@ class CartItem extends React.Component{
         // this.setState({
         //     qty:this.state.qty+1
         // });
+      
 
-        //set state form 2
+        //set state form 2 -> if previous state required
+        
         this.setState((prevState)=>{
             return {
                 qty:prevState.qty+1
+            }
+        },()=>{
+            console.log(this.state);
+        })
+       
+    }
+    decreaseQuantity=()=>{
+        var qty=this.state.qty;
+        if(qty===0){
+            return;
+        }
+
+        this.setState((prevState)=>{
+            return {
+                    qty:prevState.qty-1
+                
+                
             }
         })
     }
@@ -38,7 +57,7 @@ class CartItem extends React.Component{
                     <div style={{fontSize:25}}>{title}</div>
                     <div style={{color:'#777'}}> Rs {price}</div>
                     <div style={{color:'#888'}}>Qty : {qty}</div>
-                 </div>
+                 
 
                  <div className="cart-item-actions">
                      {/*BUttons */}
@@ -52,12 +71,14 @@ class CartItem extends React.Component{
                         alt="decrease" 
                         className="action-icons" 
                         src="https://www.flaticon.com/premium-icon/icons/svg/2985/2985073.svg"
+                        onClick={this.decreaseQuantity}
                     />
                     <img 
                         alt="delete" 
                         className="action-icons" 
                         src="https://www.flaticon.com/premium-icon/icons/svg/3106/3106795.svg"
                     />
+                 </div>
                  </div>
 
             </div>
